7821f7a1d58861977af274486d323ef00cfc0efa
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-08-06 09:54:01 +0200

    fix: prevent layout shifts due to font swapping in astro-react

--- files/framework/astro-react/astro.config.mjs
+++ files/framework/astro-react/astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Lato',
+      cssVariable: '--font-lato',
+      weights: [400, 700],
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Inconsolata',
+      cssVariable: '--font-inconsolata',
+      weights: [400, 700],
+    },
+  ],
 });
